87afcb8bf264f2c9652199c7fa777d0787585410
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2023-09-10 10:03:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+// const wildcardMatch = require('wildcard-match');
+
+const app = express();
+require("dotenv").config();
+
+const puppeteer = require("puppeteer");
+
+app.get("/", async (req, res) => {
+  let productUrl = req.query.url;
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH, // Use environment variable
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    timeout: process.env.PUPPETEER_TIMEOUT || 60000,
+  });
+  const page = await browser.newPage();
+
+  //   // Navigate to the product page
+  await page.goto(productUrl);
+  await page.waitForSelector("#productTitle", { timeout: 0 });
+
+  const productData = await page.evaluate(() => {
+    const title = document.querySelector("#productTitle").innerText.trim();
+    const price = document
+      .querySelector(".a-price .a-offscreen")
+      .innerText.trim();
+    const availability = document
+      .querySelector("#availability")
+      .innerText.trim();
+    const description = document
+      .querySelector("#productDescription")
+      .innerText.trim();
+
+    return {
+      title,
+      price,
+      availability,
+      description,
+    };
+  });
+
+  await browser.close();
+  res.json(productData);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
